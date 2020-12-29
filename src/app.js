b02b8561635da1684a75698bc3e4d91785926882
Akarsh <akarsh.saxena30@gmail.com>
2020-12-29 13:03:10 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather app',
+        title: 'Weather ',
         name: ' Akarsh Saxena'
     })
 
